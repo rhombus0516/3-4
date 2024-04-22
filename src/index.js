0d714015202423d863b5978e294e15b2a20b14e8
rhombus0516 <karaya0913@gmail.com>
2024-04-22 22:04:48 +0900

    [Add]クリック

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
-const onClickAdd = () => {
+//const onClickAdd = () => {
+//}
+
+const addButton = document.getElementById('add-button');
+const input = document.getElementById('add-text');
+const incomoAreaList = document.getElementById('incomp-area-list');
+
+addButton.addEventListener("click", function(event){
+   const task = input.ariaValueMax.trim();
+   if(task) {
+    addTask(task)
+        input.value = '';
+   } 
+});
+
+function addTask(task){
+    const listItem = document.createElement('li');
+    listItem.textContent = task;
+
+    const conpleteButton = document.createElement('button');
+    const deleteButton = document.createElement('button');
+
+    conpleteButton.textContent = "完了";
+    deleteButton.textContent = "削除";
 }
