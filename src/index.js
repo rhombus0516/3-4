f5037b9737292e0c17c2333545bfc647aed9c17d
rhombus0516 <karaya0913@gmail.com>
2024-04-23 21:30:55 +0900

    [Add]todo

--- src/index.js
+++ src/index.js
@@ -1,25 +1,58 @@
 //const onClickAdd = () => {
 //}
-
 const addButton = document.getElementById('add-button');
-const input = document.getElementById('add-text');
-const incomoAreaList = document.getElementById('incomp-area-list');
+const input = document.getElementById('add-text')
+const incompAreaList = document.getElementById('incomp-area-list')
 
-addButton.addEventListener("click", function(event){
-   const task = input.ariaValueMax.trim();
-   if(task) {
-    addTask(task)
+//task追加
+addButton.addEventListener('click', function(event){
+    const task = input.value.trim();
+    if (task) {
+        addTask(task);
         input.value = '';
-   } 
+    }
 });
 
-function addTask(task){
+//リスト
+function addTask(task) {
     const listItem = document.createElement('li');
     listItem.textContent = task;
 
-    const conpleteButton = document.createElement('button');
+    const completeButton = document.createElement('button');
     const deleteButton = document.createElement('button');
 
-    conpleteButton.textContent = "完了";
-    deleteButton.textContent = "削除";
+    completeButton.textContent = '完了';
+    deleteButton.textContent = '削除';
+    
+    completeButton.addEventListener('click', function() {
+        const parent = this.parentElement; 
+        parent.remove(); 
+        addCompletedTask(task); 
+    });
+
+    deleteButton.addEventListener('click', function(){
+        const parent = this.parentElement; 
+        parent.remove(); 
+    });
+
+    listItem.appendChild(completeButton); 
+    listItem.appendChild(deleteButton); 
+    
+    incompAreaList.appendChild(listItem);
+
+}
+function addCompletedTask(task) {
+    const listItem = document.createElement('li');
+    listItem.textContent = task;
+
+    const backButton = document.createElement('button');
+    backButton.textContent = '戻す';
+    backButton.addEventListener('click', function() {
+        const parent = this.parentElement;
+        parent.remove();
+        addTask(task)
+    });
+
+    listItem.appendChild(backButton);
+    document.getElementById('comp-area-list').appendChild(listItem);
 }
